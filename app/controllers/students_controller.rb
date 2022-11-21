098cff1a01e11e1365ed0a92420f615e21066a1d
yego <yegokipkemoi254@gmail.com>
2022-11-21 21:54:22 +0300

    Done

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,16 @@
+class StudentsController < ApplicationController
+   def index
+    students = Student.all
+    render json: students
+   end 
+
+   def grades
+    grades = Student.order(grade: :desc)
+    render json: grades
+   end
+
+   def highest_grade
+    highest_grade = Student.order(grade: :desc).first
+    render json: highest_grade
+   end
+end
